c8a4f977f4fdf49fcea21435086a086741220f89
AngryBandera <52661805+AngryBandera@users.noreply.github.com>
2025-04-06 22:28:44 +0300

    working code

--- Maximum Frequency Stack/main.py
+++ Maximum Frequency Stack/main.py
@@ -4,21 +4,18 @@ class FreqStack:
 
     def __init__(self):
         self.freq = defaultdict(int)
+        self.group = defaultdict(list)
+        self.max_freq = 0
 
     def push(self, val: int) -> None:
-        self.dict[val] += 1
-        self.max
+        self.freq[val] += 1
+        f = self.freq[val]
+        self.group[f].append(val)
+        self.max_freq = max(self.max_freq, f)
 
     def pop(self) -> int:
-        freq = 0
-        freq_count = 0
-        for key in self.dict.keys():
-            if self.dict[key] > freq_count:
-
-                freq = key
-                freq_count = self.dict[key]
-                print(key)
-                print(self.dict)
-        self.dict[freq] -= 1
-        return freq
-                
+        val = self.group[self.max_freq].pop()
+        self.freq[val] -= 1
+        if not self.group[self.max_freq]:
+            self.max_freq -= 1
+        return val
